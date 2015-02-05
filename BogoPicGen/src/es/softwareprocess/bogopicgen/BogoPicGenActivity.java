89eb6a2e7ee8eb8d544c289dcf4aa1e91e00962d
yeni94 <omoyeni@ualberta.ca>
2015-02-05 15:21:00 -0700

    Changes for lab5

--- BogoPicGen/src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
+++ BogoPicGen/src/es/softwareprocess/bogopicgen/BogoPicGenActivity.java
@@ -90,14 +90,17 @@ public class BogoPicGenActivity extends Activity {
 
 	private void setBogoPic() {
 		// TODO: Show a toast with message "Generating Photo"
+		Toast.makeText(this, "Generating Photo", Toast.LENGTH_LONG).show();
 		
 		
 		// TODO: Get a reference to the image button
 		
 		
+		
 		// Generate a bogopic
 		ourBMP = BogoPicGen.generateBitmap(400, 400);
-		
+		ImageButton preview = (ImageButton) findViewById(R.id.TakeAPhoto);
+		preview.setImageBitmap(ourBMP);
 		// TODO: Assign the bogopic to the button with setImageBitmap
 		
 	}
@@ -113,11 +116,15 @@ public class BogoPicGenActivity extends Activity {
 			if (intent.getExtras() != null) {
 				// TODO: If cancelled, show a toast, set result to RESULT_CANCELED, finish and return 
 				
+				if (cancel == true){
+					setResult(RESULT_OK);
+					Toast.makeText(this, "Cancelled :(", Toast.LENGTH_LONG).show();
+				} else {
 				
 				// If accepted save the picture
 				File intentPicture = getPicturePath(intent);
 				saveBMP(intentPicture, ourBMP);
-				
+				}
 				// TODO: set result to RESULT_OK
 				
 			} else {
--- CameraTest/src/com/example/cameratest/MainActivity.java
+++ CameraTest/src/com/example/cameratest/MainActivity.java
@@ -45,6 +45,9 @@ public class MainActivity extends Activity {
 		// TODO: Create an intent with the action
 		// MediaStore.ACTION_IMAGE_CAPTURE
 		
+		
+		
+		
 		// ComponentName cn = new ComponentName("es.softwareprocess.bogopicgen",
 		// "es.softwareprocess.bogopicgen.BogoPicGenActivity");
 		// ComponentName cn = new ComponentName("com.android.camera",
@@ -64,6 +67,11 @@ public class MainActivity extends Activity {
 				+ String.valueOf(System.currentTimeMillis()) + ".jpg";
 		File imageFile = new File(imageFilePath);
 		imageFileUri = Uri.fromFile(imageFile);
+		
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+		
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
 		// TODO: Put in the intent in the tag MediaStore.EXTRA_OUTPUT the URI
 		
@@ -73,6 +81,21 @@ public class MainActivity extends Activity {
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE){
+			if (resultCode == RESULT_OK) {
+				TextView tv = (TextView) findViewById(R.id.status);
+				tv.setText("Photo Ok!");
+				ImageButton ib = (ImageButton) findViewById(R.id.TakeAPhoto);
+				Drawable picture = Drawable.createFromPath(imageFileUri.getPath());
+				ib.setImageDrawable(picture);
+			} else if (resultCode == RESULT_CANCELED) {
+				TextView tv = (TextView) findViewById(R.id.status);
+				tv.setText("Photo Cancelled!");
+			} else {
+				TextView tv = (TextView) findViewById(R.id.status);
+				tv.setText("Photo Ok!");
+			}
+		}
 		// TODO: Handle the results from CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE
 		
 		// TODO: Handle the cases for RESULT_OK, RESULT_CANCELLED, and others
